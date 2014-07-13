bc4c04f89633523654bf62b51432f607fc86bd8d
Kamil Mroczek <kamil.mroczek@gmail.com>
2014-07-12 21:01:20 -0700

    Random fixes
    - Made service work when app is swiped away from task manager
    - Lowered min SDK version
    - Refactored checking google play services
    - Added another point to geofences

--- app/src/main/java/com/kamil/android_location/android/GooglePlayHelper.java
+++ app/src/main/java/com/kamil/android_location/android/GooglePlayHelper.java
@@ -11,19 +11,23 @@ public class GooglePlayHelper {
 	public GooglePlayHelper() {
 		
 	}
-	
-	public boolean servicesConnected(Context context) {
-		// Check that Google Play services is available
-		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
-		// If Google Play services is available
-		if (ConnectionResult.SUCCESS == resultCode) {
-			// In debug mode, log the status
-			Log.d("Google Services", "Google Play services is available.");
-			return true;
-		} else { // Google Play services was not available for some reason
-			Log.d("Google Services", "Google Play services is NOT available.");
-			return false;
-		}
+
+    public boolean servicesConnected(Context context) {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Google Services", "Google Play services is available.");
+            return true;
+        } else { // Google Play services was not available for some reason
+            Log.d("Google Services", "Google Play services is NOT available.");
+            return false;
+        }
+    }
+
+	public int servicesResult(Context context) {
+		return  GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
 	}
 
 }
